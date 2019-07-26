bc2b24b4cdfee8974e6bbf76f57ef17e79d36d38
Jasen Jackson <jasenjackson@dhcp-10-101-198-175.wireless.northwestern.private>
2019-07-26 17:13:52 -0500

    added main function to cellQC

--- cellQC.r
+++ cellQC.r
@@ -7,22 +7,20 @@
 #        Rscript cellQC.r --matrix /data/matrix.txt.gz
 
 suppressPackageStartupMessages(require(optparse))
-# manual: http://cran.r-project.org/web/packages/optparse/optparse.pdf
-# vignette: http://www.icesi.edu.co/CRAN/web/packages/optparse/vignettes/optparse.pdf
-
 option_list = list(
   make_option(c("-m", "--matrix"), action="store", default=NA, type="character",
-              help="gene count x cell matrix")
+              help="gene count x cell matrix"),
+  make_option(c("-f", "--filetype", action="store"), default=NA, type="character")
 )
 opt = parse_args(OptionParser(option_list=option_list))
 
-print_input <- function(input) {
+main <- function(input) {
   cat("Count matrix: ")
   cat(input); cat("\n")
 }
 
 if (!is.na(opt$m)) {
-  print_input(opt$m)
+  main(opt$m)
 } else {
   cat("Please provide path for a count matrix (-m/--matrix) \n")
 }
