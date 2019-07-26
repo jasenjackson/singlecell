a55b4b05238858638d22d9a185c00fc7dea1f074
Jasen Jackson <jasenjackson@dhcp-10-101-198-175.wireless.northwestern.private>
2019-07-26 16:21:31 -0500

    created cellQC.r script

---
+++ cellQC.r
@@ -0,0 +1,25 @@
+# scRNA cell (and gene) quality control script
+# mendillolab.org
+# 7-26-2019
+# Generates QC plots from gene x cell matrix
+
+# usage: Rscript cellQC.r -m /data/matrix.txt.gz
+#        Rscript cellQC.r --matrix /data/matrix.txt.gz
+
+suppressPackageStartupMessages(require(optparse))
+# manual: http://cran.r-project.org/web/packages/optparse/optparse.pdf
+# vignette: http://www.icesi.edu.co/CRAN/web/packages/optparse/vignettes/optparse.pdf
+
+option_list = list(
+  make_option(c("-m", "--matrix"), action="store", default=NA, type="character",
+              help="gene count x cell matrix")
+)
+opt = parse_args(OptionParser(option_list=option_list))
+
+if (opt$m == "t") {
+  cat("true")
+} else if (opt$m == "f") {
+  cat("false")
+} else {
+  cat("nothing!")
+}
